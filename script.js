8715717e4bd647a4dee162cceb20bcdfa9e1668e
TheoAndreas10 <144347122+TheoAndreas10@users.noreply.github.com>
2024-12-18 18:50:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add event listener to button
+document.getElementById("clickMeButton").addEventListener("click", function() {
+    document.getElementById("message").textContent = "Tombol telah diklik! Selamat belajar!";
+});
